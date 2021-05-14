c65d2b3522daa8cc316677d8e17a3d53745a9438
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 15:18:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  function titleCase(string) {
-      var sentence = string.toLowerCase().split(" ");
+      var sentence = tutorials.toLowerCase().split(" ");
       for(var i = 0; i< sentence.length; i++){
          sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
       }
   // document.write(sentence.join(" "));
    return sentence;
-   }
-  return tutorials
 }
