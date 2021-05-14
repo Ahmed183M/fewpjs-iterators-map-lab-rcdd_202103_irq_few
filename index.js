023b8d5b1dc35e4e0a14416a582336f35d9825b0
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 15:17:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const titleCased = () => {
       for(var i = 0; i< sentence.length; i++){
          sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
       }
-   document.write(sentence.join(" "));
+   sentence.join(" ");
    return sentence;
    }
 }
